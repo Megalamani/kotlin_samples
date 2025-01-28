adf60998446e7c280feb4f0b6974ffcb4f9177a1
megala <megala@qbrik.in>
2025-01-28 15:35:45 +0530

    // first commit

--- app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
@@ -4,8 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInsta
-                          nceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
