f2abfb02e58a6ab6e5142ed8f80c7c2be0835eaa
Megalamani <megalamani593@gmail.com>
2025-01-28 15:30:22 +0530

    Add files via upload

---
+++ app/src/main/java/com/example/recyclerviewkotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.recyclerviewkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInsta
+                          nceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
